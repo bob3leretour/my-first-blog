c2b3b9cd212491840f7c996d2a0a03b45c50def6
bob3leretour <benjaminchomel@hotmail.com>
2015-07-14 23:18:08 +0800

    added posts

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 
 def post_list(request):
     user = authenticate(username='benjamin', password='kenshiro8')
+    login(request, user)
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
