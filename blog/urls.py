909716d08a23910e78747d38689ae8fe281234f5
bob3leretour <benjaminchomel@hotmail.com>
2015-07-20 16:55:35 +0800

    email and  img

--- blog/urls.py
+++ blog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^post/register/$', views.post_register, name='post_register'),
     url(r'^post/logout/$', views.post_logout, name='post_logout'),
     url(r'^post/login/$', views.post_login, name='post_login'),
-    url(r'^post/(?P<pk>[1-9]+)/$', views.post_details),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_details),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
